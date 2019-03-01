36c2123db7c40d74dcb694f73ad10b221c2982a4
Christopher Stoll <43278229+chris-stoll@users.noreply.github.com>
2019-02-28 18:37:39 -0600

    Increase ram allocation from 16 to 20 gb.
    Changed ram from 16gb to 20gb. Failed recordings due to not enough free space on transcode directory.

--- Plex_PreStartup.cmd
+++ Plex_PreStartup.cmd
@@ -1,5 +1,5 @@
 @echo off
-imdisk -a -s 16G -m Z: -p "/fs:ntfs /q /y"
+imdisk -a -s 20G -m Z: -p "/fs:ntfs /q /y"
 mkdir "Z:\PlexTranscodeTemp"
 timeout 30 > NUL
 C:\Python27\pythonw.exe "Y:\Plex Media Server\Tautulli\Tautulli.py"
